58b8e130ad0be6adb8e7f5bd8f715242e721c028
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2014-04-15 23:03:08 +0200

    Added assemble template with gh-pages builder

---
+++ Gruntfile.js
@@ -0,0 +1,101 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Grunt tasks
+  require('load-grunt-tasks')(grunt);
+  require('time-grunt')(grunt);
+
+  // Assemble build
+  grunt.loadNpmTasks('assemble');
+
+  // Project configuration.
+  grunt.initConfig({
+
+    config: {
+      src: 'src',
+      dist: 'dist'
+    },
+
+    watch: {
+      assemble: {
+        files: ['<%= config.src %>/{content,data,templates}/{,*/}*.{md,hbs,yml}'],
+        tasks: ['assemble']
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          '<%= config.dist %>/{,*/}*.html',
+          '<%= config.dist %>/assets/{,*/}*.css',
+          '<%= config.dist %>/assets/{,*/}*.js',
+          '<%= config.dist %>/assets/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
+      }
+    },
+
+    connect: {
+      options: {
+        port: 9000,
+        livereload: 35729,
+        // change this to '0.0.0.0' to access the server from outside
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          open: true,
+          base: [
+            '<%= config.dist %>'
+          ]
+        }
+      }
+    },
+
+    assemble: {
+      pages: {
+        options: {
+          flatten: true,
+          assets: '<%= config.dist %>/assets',
+          layout: '<%= config.src %>/templates/layouts/default.hbs',
+          data: '<%= config.src %>/data/*.{json,yml}',
+          partials: '<%= config.src %>/templates/partials/*.hbs',
+          plugins: ['assemble-contrib-permalinks','assemble-contrib-sitemap'],
+        },
+        files: {
+          '<%= config.dist %>/': ['<%= config.src %>/templates/pages/*.hbs']
+        }
+      }
+    },
+
+    // Before generating any new files,
+    // remove any previously-created files.
+    clean: ['<%= config.dist %>/**/*.{html,xml}'],
+
+    // Automatically create/push gh-pages
+    'gh-pages': {
+      options: {
+        base: 'dist'
+      },
+      src: ['**']
+    }
+
+  });
+
+  grunt.registerTask('server', [
+    'clean',
+    'assemble',
+    'connect:livereload',
+    'watch'
+  ]);
+
+  grunt.registerTask('build', [
+    'clean',
+    'assemble'
+  ]);
+
+  grunt.registerTask('default', [
+    'build'
+  ]);
+
+};
