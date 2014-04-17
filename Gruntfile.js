ac4cfe976081b6505b9ffb26c9abfa3a84b14520
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2014-04-17 23:58:31 +0200

    Added server deprecation

--- Gruntfile.js
+++ Gruntfile.js
@@ -289,7 +289,12 @@ module.exports = function(grunt) {
 
   });
 
-  grunt.registerTask('server', [
+  grunt.registerTask('server', function() {
+        grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
+        grunt.task.run(['serve']);
+    });
+
+  grunt.registerTask('serve', [
     'clean',
     'concurrent:server',
     'copy:dev',
